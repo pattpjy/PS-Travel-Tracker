fdb74d083e7246aa4c017cb1a6f4fed4e0e070b7
Patt Sookmark <pijiya@sookmark.com>
2023-01-06 23:29:43 -0700

    Add destination class construction

--- src/Destinations.js
+++ src/Destinations.js
@@ -1,8 +1,13 @@
 // create a destination class for a desination info
 
 class destination {
-    constructor(){
-
+    constructor(data){
+        this.id = data.id,
+        this.destination = data.destination,
+        this.estLodgingCostPerDay = data.estimatedLodgingCostPerDay,
+        this.estFlighCostPerPerson = data.estimatedFlightCostPerPerson,
+        this.image = data.image,
+        this.alt = data.alt
     }
 }
 
