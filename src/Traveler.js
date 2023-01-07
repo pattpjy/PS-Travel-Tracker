8a91fc2e0ba98240c890078360a949b42617cd34
Patt Sookmark <pijiya@sookmark.com>
2023-01-06 23:23:58 -0700

    Add traveler class construction

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,8 +1,10 @@
 //create a traveller class for a traveler info
 
 class Traveler  {
-    constructor () {
-        
+    constructor (data) {
+        this.id = data.id,
+        this.name = data.name,
+        this.travelerType = data.travelerType
     }
 }
 
