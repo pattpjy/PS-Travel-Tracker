a42dc91b352de47845e466e5b56924671a457a6b
Patt Sookmark <pijiya@sookmark.com>
2023-01-07 12:47:29 -0700

    Add psuedo code to TripRepo method and unit test

--- src/TripRepo.js
+++ src/TripRepo.js
@@ -1,21 +1,42 @@
 // create a repository for trips related info
 //Trip repo will have all information related to all trips
-
+import Trip from './src/Trip.js'
 class TripRepo {
-    constructor () {
-
+    constructor (data) {
+        this.tripData = data.map(el => new Trip(el))
     }
 
 //All of my trips (past, upcoming and pending)
 //Total amount I have spent on trips this year. This should be calculated from the trips data and include a travel agent’s 10% fee
 
-// function getPastTripsByUserID()
+ getPastTripsByUserID(userId) {
+    //loop thru this.tripData 
+        //filter by userId and date < current date
+        // return trips object 
+    console.log('hello')
+}
 
-// function getUpcomingTripsByUserID()
+getUpcomingTripsByUserID(userId) {
+    //loop thru this.tripData 
+        //filter by userId and date >= current date
+        //return trips object 
+}
 
-// function getPendingTripByUserID()
+getPendingTripByUserID(userId) {
+    //loop thru this.tripData 
+        //filter by userId and status === pending
+        //return trips object 
+}
 
-// function calcTripCostCurrYear()
+calcTripCostCurrYear(userId) {
+    //loop thru this.tripData
+        //filter by userId
+    //for each trips and  loop destination data
+        //if destinationID match
+            // estimatedLodgingCostPerDay * trips.duration
+            //estimatedFlightCostPerPerson * trips.travelers
+        // return a single number
+}
 
 
 }
