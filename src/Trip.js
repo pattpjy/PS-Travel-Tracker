ef045cf4cd8a4725b15290bd7d95c63982d68a9e
Patt Sookmark <pijiya@sookmark.com>
2023-01-06 23:21:59 -0700

    Add trip class constructor

--- src/Trip.js
+++ src/Trip.js
@@ -1,8 +1,15 @@
 // create trip class to store a trip info
 
 class Trip {
-    constructor () {
-
+    constructor (data) {
+        this.id = data.id,
+        this.userID = data.userID,
+        this.destinationID = data.destinationID,
+        this.travelers = data.travelers,
+        this.date = data.date,
+        this.duration = data.duration,
+        this.status = data.status,
+        this.suggestedActivities = data.suggestedActivities
     }
 }
 
