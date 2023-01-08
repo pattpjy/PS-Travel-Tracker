8f2b1a7fd7d7aebdc7b3653060e37f961171df24
Patt Sookmark <pijiya@sookmark.com>
2023-01-08 00:16:24 -0700

    Add Traveler test and class

---
+++ test/Traveler-test.js
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import travelers from "../src/data/data-traveler"
+import Traveler from "../src/Traveler"
+
+describe("Traveler Repository", () => {
+    let traveler;
+ 
+    beforeEach(() => {
+        traveler = new Traveler(travelers[0]);
+      });
+
+    it("should be a function", function () {
+      expect(Traveler).to.be.a("function");
+    });
+
+    it("Should be instance of userRepository", function () {
+      expect(traveler).to.be.an.instanceOf(Traveler);
+    });
+
+    it("should have properties that holds the traveler info", function () {
+        expect(traveler.id).to.equal(1);
+      });
+})
