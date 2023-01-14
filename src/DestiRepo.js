ff77fd9208859811fd54d7dc3fe6e5e331c5876e
Patt Sookmark <pijiya@sookmark.com>
2023-01-14 04:37:10 -0700

    Add destinationRepo class/method

---
+++ src/DestiRepo.js
@@ -0,0 +1,19 @@
+// this is where to instanciate new Destination
+// this repo will act on aggregate destination
+
+// method to find destination by ID
+import Destination from './Destinations'
+
+
+class DestiRepo {
+    constructor (data) {
+        this.destiData = data.map(obj => new Destination(obj))
+    }
+    // method to find destination by ID
+    getDestinationById(destId) {
+        const destiFound = this.destiData.filter((obj)=> {
+            return obj.id === destId
+        })
+        return destiFound
+    }
+}
